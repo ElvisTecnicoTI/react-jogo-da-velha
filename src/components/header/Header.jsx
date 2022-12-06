2e0452f0c5cd292922946b31ed3e56197066367f
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2022-12-06 13:29:13 -0300

    criei o componente do ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
 import styles from './Header.module.css'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Elvis Ferreira</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/ElvisTecnicoTI" />
+      </div>
     </div>
 
   )
