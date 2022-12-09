fd3247409d5b8c0120b8b3818b16f784fd486d9f
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2022-12-09 17:14:46 -0300

    criei o status no gameoption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,10 +1,21 @@
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
+import { useState } from 'react'
 
 
 function Game () {
+  const [gameState, setGameState] = useState(Array(8).fill(0))
+
   return (
     <div className={styles.game}>
+      {
+        gameState.map((value, pos) =>
+          <GameOption
+            key={'game-option-pos-${pos}'}
+            status={value}
+          />
+        )
+      }
       <GameOption /> <GameOption /> <GameOption />
       <GameOption /> <GameOption /> <GameOption />
       <GameOption /> <GameOption /> <GameOption />
